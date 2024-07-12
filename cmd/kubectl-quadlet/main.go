6c741c694ce60e0a3f8db6967c466d0596bcc15d
guppy0130 <guppy0130@yahoo.com>
2024-07-11 22:40:11 -0500

    initial commit from template

---
+++ cmd/kubectl-quadlet/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+var (
+	KubernetesConfigFlags *genericclioptions.ConfigFlags
+)
+
+func RootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "kubectl quadlet",
+		Short:         "create quadlet files from kubernetes manifests",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			viper.BindPFlags(cmd.Flags())
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log := slog.New(slog.NewTextHandler(os.Stderr, nil))
+			log.Info("Hello world")
+
+			return nil
+		},
+	}
+
+	cobra.OnInitialize(initConfig)
+
+	KubernetesConfigFlags = genericclioptions.NewConfigFlags(false)
+	KubernetesConfigFlags.AddFlags(cmd.Flags())
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	return cmd
+}
+
+func initConfig() {
+	viper.AutomaticEnv()
+}
+
+func main() {
+	if err := RootCmd().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
